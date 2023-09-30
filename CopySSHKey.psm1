65d73975b45e687c38aa592d33f7c9fd2d4bd27e
SeryiBaran <seryibaranminepy@yandex.ru>
2023-09-30 22:04:59 +0300

    2.0

--- CopySSHKey.psm1
+++ CopySSHKey.psm1
@@ -35,7 +35,7 @@ function Copy-SSHKey {
 
   try {
 #    ssh $RemoteHost -p $RemotePort "mkdir ~/.ssh"
-    Get-Content $KeyFile | ssh $RemoteHost -p $RemotePort "mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys"
+    ssh $RemoteHost -p $RemotePort "mkdir ~/.ssh; chmod 700 ~/.ssh; touch ~/.ssh/authorized_keys; chmod 600 ~/.ssh/authorized_keys; echo "$(Get-Content $KeyFile)" >> ~/.ssh/authorized_keys"
   }
   catch {
     Write-Error "Oh, error occurred:"
